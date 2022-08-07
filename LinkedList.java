921f7a35cab6a01af2df1fa322926a1954059575
Anusha <anushavysakh232@gmail.com>
2022-08-08 00:54:21 +0530

    Uc4 committed

--- LinkedList.java
+++ LinkedList.java
@@ -1,17 +1,17 @@
 package com.day14linkedlist.bridglabz;
 
-public class LinkedList3<T> {
+public class LinkedList4<T> {
 	public Node<T> head = null;
 	public Node<T> tail = null;
 
 	public void addFirst(T data) {
 		if (isEmpty()) {
 
-			head = new Node(data);
+			head = new Node<T>(data);
 			tail = head;
 			return;
 		}
-		Node newNode = new Node(data);
+		Node<T> newNode = new Node<T>(data);
 		newNode.next = head;
 		head = newNode;
 	}
@@ -19,29 +19,26 @@ public class LinkedList3<T> {
 	public boolean isEmpty() {
 		return head == null;
 	}
-//Adding element to last 
-	public void addLast(T data) {
-		if (isEmpty()) {
 
-			head = new Node(data);
-			tail = head;
+	public void insertAfter(Node prev_node, int new_data) {
+		if (prev_node == null) {
+			System.out.println("The given previous node cannot be null");
 			return;
 		}
-		Node newNode = new Node(data);
-		tail.next = newNode;
-		tail = newNode;
+		Node new_node = new Node(new_data);
+		new_node.next = prev_node.next;
+
+		prev_node.next = new_node;
 	}
-	
+
 	void display() {
 		Node<T> current = head;
 
 		while (current != null) {
-			System.out.print(current.data+"->"  );
+			System.out.print(current.data + "->");
 			current = current.next;
 		}
 		System.out.println("null");
 	}
 
 }
-
-
--- Main.java
+++ Main.java
@@ -1,17 +1,16 @@
 package com.day14linkedlist.bridglabz;
 
-public class Main3 {
+public class Main4 {
 	public static void main(String args[]) {
-	 LinkedList3<Integer> list = new LinkedList3<>();
-     list.display();
-     list.addFirst(56);
-	 System.out.println("Node with data 56 is First Created");
-     list.display();
-      System.out.println("Next 30 is added to 70");
-     list.addLast(30);
-     list.display();
-     System.out.println("Next 56 is added to 70");
-     list.addLast(70);
-     list.display();
+		 LinkedList4<Integer> list = new LinkedList4<>();
+	     list.display();
+	     list.addFirst(70);
+		 System.out.println("Added 56 ");
+		 list.addFirst(56);
+		 System.out.println("Added 70");
+		 list.display();
+		 System.out.println("Adding 30 between 70 and 56");
+		 list.insertAfter(list.head,30);
+		 list.display();
 }
 }
