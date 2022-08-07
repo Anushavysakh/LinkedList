9683a2cb0985b7df6a5599424f5c20948e0340da
Anusha <anushavysakh232@gmail.com>
2022-08-08 01:04:14 +0530

    Uc8 committed

--- LinkedList.java
+++ LinkedList.java
@@ -1,6 +1,6 @@
 package com.day14linkedlist.bridglabz;
 
-public class LinkedList7<T> {
+public class LinkedList8<T> {
 	public Node<T> head = null;
 	public Node<T> tail = null;
 
@@ -12,7 +12,7 @@ public class LinkedList7<T> {
 			return;
 		}
 		Node<T> newNode = new Node<T>(data);
-		
+
 		newNode.next = head;
 		head = newNode;
 	}
@@ -30,41 +30,55 @@ public class LinkedList7<T> {
 		}
 		System.out.println("null");
 	}
-	
+
 	public void pop() {
-		 Node current = head;
+		Node current = head;
 
-	      while(current.next.next != null) {
-	          current = current.next;
-	      }
+		while (current.next.next != null) {
+			current = current.next;
+		}
 
-	      current.next = null;
+		current.next = null;
 
-	  //    System.out.println(current);
-	}
-	
-	 public void searchNode(T data) {  
-	        Node current = head;  
-	        int i = 1;  
-	        boolean flag = false;  
-	        //Checks whether list is empty  
-	        if(head == null) {  
-	            System.out.println("List is empty");  
-	        }  
-	        else {  
-	            while(current != null) {  
-	                if(current.data == data) {  
-	                    flag = true;  
-	                    break;  
-	                }  
-	                i++;  
-	                current = current.next;  
-	            }  
-	        }  
-	        if(flag)  
-	             System.out.println("Element is present in the list at the position : " + i);  
-	        else  
-	             System.out.println("Element is not present in the list");  
-	    }  
+		// System.out.println(current);
 	}
 
+	public void searchNode(T data) {
+		Node current = head;
+		int i = 1;
+		boolean flag = false;
+		// Checks whether list is empty
+		if (head == null) {
+			System.out.println("List is empty");
+		} else {
+			while (current != null) {
+				if (current.data == data) {
+					flag = true;
+					break;
+				}
+				i++;
+				current = current.next;
+			}
+		}
+		if (flag)
+			System.out.println("Element is present in the list at the position : " + i);
+		else
+			System.out.println("Element is not present in the list");
+	}
+
+	public void insertAfter(Node prev_node, int new_data) {
+		if (prev_node == null) {
+			System.out.println("The given previous node cannot be null");
+			return;
+		}
+
+		Node new_node = new Node(new_data);
+
+		
+		new_node.next = prev_node.next;
+
+		/* 5. make next of prev_node as new_node */
+		prev_node.next = new_node;
+	}
+
+}
--- Main.java
+++ Main.java
@@ -1,32 +1,19 @@
 package com.day14linkedlist.bridglabz;
 
-public class Main7 {
+public class Main8 {
 
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 
-		LinkedList7<Integer> list = new LinkedList7<>();
+		LinkedList8<Integer> list = new LinkedList8<>();
 
 		System.out.println("Added 70 to LinkedList");
 		list.addFirst(70);
-		list.display();
-		
-		System.out.println("Adding 30 to LinkedList");
 		list.addFirst(30);
-		list.display();
-		
-		System.out.println("Adding 56 to LinkedList");
 		list.addFirst(56);
 		list.display();
-		
-		System.out.println("The last element 70  is removed:\t");
-		list.pop();
+		list.insertAfter(list.head.next, 40);	
 		list.display();
 		
-		System.out.println("Added 68 to LinkedList");
-		list.addFirst(68);
-		list.display();
-		
-		System.out.println("Search for node 30");
-		list.searchNode(30);
 	}
+
 }
