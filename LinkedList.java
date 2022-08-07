402eeae5b4fb6d30eacef17c2d0841304210e593
Anusha <anushavysakh232@gmail.com>
2022-08-08 00:47:47 +0530

    Uc2 committed

--- LinkedList.java
+++ LinkedList.java
@@ -1,26 +1,31 @@
 package com.day14linkedlist.bridglabz;
 
-public class LinkedList<T> {
+public class LinkedList2<T> {
 	public Node<T> head = null;
 	public Node<T> tail = null;
 
-	void add(T data) {
-		Node<T> newNode = new Node<>(data);
-		if (head == null) {
-			head = newNode;
-			tail = newNode;
-		} else {
+	public void addFirst(T data) {
+		if (isEmpty()) {
 
-			tail.next = newNode;
-			tail = newNode;
+			head = new Node(data);
+			tail = head;
+			return;
 		}
+		Node newNode = new Node(data);
+		newNode.next = head;
+		head = newNode;
 	}
 
+	public boolean isEmpty() {
+		return head == null;
+	}
+
+	
 	void display() {
 		Node<T> current = head;
 
 		while (current != null) {
-			System.out.print(current.data + "->");
+			System.out.print(current.data+"->"  );
 			current = current.next;
 		}
 		System.out.println("null");
--- Main.java
+++ Main.java
@@ -1,17 +1,20 @@
 package com.day14linkedlist.bridglabz;
 
-public class Main {
+public class Main2 {
+	 public static void main(String[] args) {
+    	 LinkedList2<Integer> list = new LinkedList2<>();
+    	 list.addFirst(null);
+         list.display();
+         list.addFirst(70);
+    	 System.out.println("Node with data 70 is First Created");
+         list.display();
+          System.out.println("Next 30 is added to 70");
+         list.addFirst(30);
+         list.display();
+         System.out.println("Next 56 is added to 70");
+         list.addFirst(56);
+         list.display();
 
-	public static void main(String[] args) {
-
-		
-		
-		LinkedList<Integer> list = new LinkedList<Integer>();
-		list.add(56);
-		list.add(30);
-		list.add(70);
-		
-		list.display();
-		
-	}
+         
+	 }
 }
